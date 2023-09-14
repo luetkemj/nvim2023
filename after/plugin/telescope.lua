03a02c5aa8ebb81c84a3786dd37e994f78186e6e
luetkemj <luetkemj@gmail.com>
2023-09-14 09:34:00 -0600

    telescope vert layout

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,13 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require("telescope").setup({
+  defaults = {
+    layout_strategy='vertical',
+    layout_config = {
+      vertical = { width = 0.75 }
+      -- other layout configuration here
+    },
+  },
+})
