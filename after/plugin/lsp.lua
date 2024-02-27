b8a17c9342b34bf918560b2759c1fbfd0a30c861
Mark Luetke <luetkemj@gmail.com>
2024-02-27 15:28:41 -0700

    tab complete - FINALLY

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,21 @@
 local lsp = require('lsp-zero').preset({})
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
 lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp.default_keymaps({buffer = bufnr})
 end)
+
+-- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/autocomplete.md#enable-super-tab
+-- If the completion menu is visible it will navigate to the next item in the list. If the cursor is on top of a "snippet trigger" it'll expand it. If the cursor can jump to a snippet placeholder, it moves to it. If the cursor is in the middle of a word it displays the completion menu. Else, it acts like a regular Tab key.
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+  }),
+})
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
